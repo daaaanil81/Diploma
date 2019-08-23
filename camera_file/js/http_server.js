16056b863b7634496ff27f67100aaf3b5dd36814
Daniil Petrov <daaaanil81@gmail.com>
2019-08-23 19:49:47 +0300

    Change files for server

--- camera_file/js/http_server.js
+++ camera_file/js/http_server.js
@@ -1,19 +1,56 @@
-const n_static = require('node-static');
 const fs = require('fs');
-const file = new n_static.Server('./index.html');
+const url = require('url');
+const path = require('path');
 const host = process.argv.slice(2)[0];
 const port = 8888;
-const app = require('https').createServer({
-    key: fs.readFileSync('./certificate/test_cer.key'),
-    cert: fs.readFileSync('./certificate/test_cer.crt')
-},handler);
-app.listen(port, host,function () {
-    console.log('Server listening to ',host, ':', port);
+const mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword',
+    '.eot': 'appliaction/vnd.ms-fontobject',
+    '.ttf': 'aplication/font-sfnt'
+};
+const app = require('http').createServer(handler);
+app.listen(port, host, function () {
+    console.log('Server listening to ', host, ':', port);
 });
-function handler (req, res) {
-    file.serve(req, res);
+
+function handler(req, res) {
+    console.log(`${req.method} ${req.url}`);
+    const parsedUrl = url.parse(req.url);
+    //console.log(parsedUrl);
+    const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
+    //console.log(__dirname);
+    var pathname = __dirname.substr(0, __dirname.length - 3);
+    pathname = path.join(pathname, sanitizePath);
+    //console.log(pathname);
+    fs.exists(pathname, function (exist) {
+        if (!exist) {
+            res.statusCode = 404;
+            res.end(`File ${pathname} not found!`);
+            return;
+        }
+        if (fs.statSync(pathname).isDirectory()) {
+            pathname += '/Sites/index.html';
+        }
+        fs.readFile(pathname, function (err, data) {
+            const ext = path.parse(pathname).ext;
+            res.setHeader('Content-type', mimeType[ext] || 'text/plain');
+            res.end(data);
+        });
+    });
 }
+
 process.stdin.resume();
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
     process.exit();
 });
--- camera_file/js/tcp_client.js
+++ camera_file/js/tcp_client.js
@@ -1,6 +1,7 @@
-var connection = new WebSocket('wcs://10.168.75.94:8555'); // tcp server on c/c++
+var connection = new WebSocket('ws://10.168.75.95:8555'); // tcp server on c/c++
 connection.onopen = function () {
   connection.send('10.168.0.85'); // Send the message 'Ping' to the server
+  console.log("Send");
 };
 connection.onclose = function(event) {
 };
