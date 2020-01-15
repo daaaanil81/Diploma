1ddcc9037db4566a059f69dec1d4f295144efc5c
daaaanil81 <daaaanil81@gmail.com>
2020-01-15 12:14:50 +0200

    Add change

--- camera_file/tcp_server/base64.h
+++ camera_file/tcp_server/base64.h
@@ -1,8 +1,17 @@
-#ifndef _BASE64_
-#define _BASE64_
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+static const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static int b64invs[] = { 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58,
+	59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5,
+	6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+	21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28,
+	29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+	43, 44, 45, 46, 47, 48, 49, 50, 51 };
 
-size_t base64_decode(char* in, char* out);
-size_t base64_encode(char* in, char* out);
-#endif
+size_t b64_encoded_size(size_t inlen);
+char *b64_encode(const unsigned char *in, size_t len);
+size_t b64_decoded_size(const char *in);
+void b64_generate_decode_table();
+int b64_isvalidchar(char c);
+int b64_decode(const char *in, unsigned char *out, size_t outlen);
